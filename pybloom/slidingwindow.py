587cb86f086ee95ed56ad7d51d678ae56a86db96
Martin Laprise <mlaprise@gmail.com>
2013-04-26 16:24:59 -0400

    Updates SlidingWindowScalableBloomFilter

--- pybloom/slidingwindow.py
+++ pybloom/slidingwindow.py
@@ -3,32 +3,48 @@ import time
 from collections import deque
 from pybloom import ScalableBloomFilter
 
+VALID_RES = {'Min': 60,
+             'Hour': 3600,
+             'Day': 86400}
 
 class DecayScalableBloomFilter(ScalableBloomFilter):
     '''
     Stepwise decaying Bloom Filter
     '''
-    def __init__(self, initial_capacity=1000, error_rate=0.01, window_period = 60):
+    def __init__(self, initial_capacity=1000, error_rate=0.01, window_period = '10_Min'):
         super(DecayScalableBloomFilter, self).__init__(initial_capacity, error_rate)
         self.window_period = 60
         self.timestamp = time.time()
+        self._setup_window_period(window_period)
         self._expired = False
+        self._locked = False
 
     @property
     def expired(self):
-        if time.time() - self.timestamp > self.window_period:
+        if time.time() - self.timestamp > self.expiration:
             self._expired = True
         return self._expired
 
+    @property
+    def locked(self):
+        if time.time() - self.timestamp > self.window_period:
+            self._locked = True
+        return self._locked
+
+    def _setup_window_period(self, window_period):
+        try:
+            self.amount, self.res = window_period.split('_')
+            self.amount = int(self.amount)
+        except ValueError:
+            raise Exception('Invalid window period')
+        self.window_period = VALID_RES[self.res]
+        self.expiration = self.amount * VALID_RES[self.res]
+        self.filters = deque(maxlen = self.amount)
 
 class SlidingWindowScalableBloomFilter(object):
     '''
     Sliding Window Bloom Filter using a coarse expiration
     '''
-
-    VALID_RES = {'Min': 60,
-                 'Hour': 3600,
-                 'Day': 86400}
 
     def __init__(self, initial_capacity=1000, window_period = "10_Min"):
         self.initial_capacity = initial_capacity
@@ -41,7 +57,10 @@ class SlidingWindowScalableBloomFilter(object):
             self.amount = int(self.amount)
         except ValueError:
             raise Exception('Invalid window period')
-        self.window_period = SlidingWindowScalableBloomFilter.VALID_RES[self.res]
+        self.window_period = self.amount * VALID_RES[self.res]
+        self._reset_filters()
+
+    def _reset_filters(self):
         self.filters = deque(maxlen = self.amount)
 
     def total_error(self):
@@ -63,21 +82,27 @@ class SlidingWindowScalableBloomFilter(object):
         if filter.expired:
             filter = DecayScalableBloomFilter(initial_capacity=self.initial_capacity,
                                               error_rate=self.error_rate,
-                                              window_period=self.window_period)
+                                              window_period="%s_%s" % (str(self.amount),self.res))
             self.filters.append(filter)
 
-
     def add(self, key):
-        self.check_expiration()
         if key in self:
             return True
         if not self.filters:
             filter = DecayScalableBloomFilter(initial_capacity=self.initial_capacity,
                                               error_rate=self.error_rate,
-                                              window_period=self.window_period)
+                                              window_period="%s_%s" % (str(self.amount),self.res))
             self.filters.append(filter)
         else:
             filter = self.filters[-1]
+            if filters.locked:
+                if filters.expired:
+                    self._reset_filters()
+                filter = DecayScalableBloomFilter(initial_capacity=self.initial_capacity,
+                                              error_rate=self.error_rate,
+                                              window_period="%s_%s" % (str(self.amount),self.res))
+                self.filters.append(filter)
+
         filter.add(key)
         return False
 
