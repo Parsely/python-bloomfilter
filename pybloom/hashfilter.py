ec5cb4e878dae00bb6b23965c6c466ee29727583
Martin Laprise <mlaprise@gmail.com>
2013-05-02 11:32:53 -0400

    Update HashFilter

--- pybloom/hashfilter.py
+++ pybloom/hashfilter.py
@@ -9,15 +9,16 @@ class HashFilter(object):
         self.unique_items = {}
 
     def add(self, key, timestamp = None):
+        timestamp = int(timestamp)
         if key in self.unique_items:
-            if not timestamp:
-                timestamp = time.time()
-            self.unique_items[key] = int(timestamp) + self.expiration
-            return True
+            if timestamp < self.unique_items[key]:
+                self.unique_items[key] = timestamp + self.expiration
+                return True
+            else:
+                self.unique_items[key] = timestamp + self.expiration
+                return False
         else:
-            if not timestamp:
-                timestamp = time.time()
-            self.unique_items[key] = int(timestamp) + self.expiration
+            self.unique_items[key] = timestamp + self.expiration
             return False
 
     def contains(self, key, timestamp):
@@ -28,13 +29,3 @@ class HashFilter(object):
             else:
                 del self.unique_items[key]
                 return False
-
-    def __contains__(self, key):
-        timestamp = time.time()
-        if key in self.unique_items:
-            if timestamp < self.unique_items[key]:
-                return True
-            else:
-                del self.unique_items[key]
-                return False
-
