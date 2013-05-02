ebbc05362e558f4cd8f37f692ac0ca95a0515766
Martin Laprise <mlaprise@gmail.com>
2013-05-01 20:32:39 -0400

    Update HashFilter

--- pybloom/hashfilter.py
+++ pybloom/hashfilter.py
@@ -2,21 +2,32 @@ import time
 
 class HashFilter(object):
     '''
-    Plain Hash Filter for testing purposes
+    Plain Temporal Hash Filter for testing purposes
     '''
     def __init__(self, expiration):
         self.expiration = expiration
         self.unique_items = {}
 
-    def add(self, key):
+    def add(self, key, timestamp = None):
         if key in self.unique_items:
-            timestamp = time.time()
-            self.unique_items[key] = timestamp + self.expiration
+            if not timestamp:
+                timestamp = time.time()
+            self.unique_items[key] = int(timestamp) + self.expiration
             return True
         else:
-            timestamp = time.time()
-            self.unique_items[key] = timestamp + self.expiration
+            if not timestamp:
+                timestamp = time.time()
+            self.unique_items[key] = int(timestamp) + self.expiration
             return False
+
+    def contains(self, key, timestamp):
+        timestamp = int(timestamp)
+        if key in self.unique_items:
+            if timestamp < self.unique_items[key]:
+                return True
+            else:
+                del self.unique_items[key]
+                return False
 
     def __contains__(self, key):
         timestamp = time.time()
